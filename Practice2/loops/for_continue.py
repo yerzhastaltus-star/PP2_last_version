0cb76aa1435ab203574289b7ec7a18828140121e
taltusee <e_taltus@kbtu.kz>
2026-02-19 08:09:54 +0500

    added file

---
+++ Practice2/loops/for_continue.py
@@ -0,0 +1,26 @@
+#1
+for a in range(5):
+    if a == 2:
+        continue
+    print(a)
+#2
+for a in range(1, 7):
+    if a % 2 == 0:
+        continue
+    print(a)
+#3
+a = int(input())
+for i in range(a):
+    if i == 1:
+        continue
+    print(i)
+#4
+for a in range(10):
+    if a == 6:
+        continue
+    print(a)
+#5
+for a in range(1, 6):
+    if a == 3:
+        continue
+    print(a)
