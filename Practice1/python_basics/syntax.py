a24c2dd38351c151ed372456214fd0c7e112056b
taltusee <e_taltus@kbtu.kz>
2026-02-19 08:27:50 +0500

    added file

---
+++ Practice1/python_basics/syntax.py
@@ -0,0 +1,14 @@
+if 5 > 2:
+  print("Five is greater than two!")
+
+if 2<4:
+    print("Two is smaller than three!")
+
+if 5!=0:
+    print("Five is not equal zero")
+
+if 1==1:
+    print("One is equal one")
+
+if 10>=10:
+    print("Ten is equal ten!")
