6e1fbcd29805f31d453d4af3000f712d7be65d59
taltusee <e_taltus@kbtu.kz>
2026-02-19 08:07:43 +0500

    added file

---
+++ Practice2/loops/for_loop.py
@@ -0,0 +1,16 @@
+#1
+for a in range(5):
+    print(a)
+#2
+for a in range(1, 6):
+    print(a)
+#3
+a = int(input())
+for i in range(a):
+    print(i)
+#4
+for a in range(10, 0, -1):
+    print(a)
+#5
+for a in range(0, 10, 2):
+    print(a)
