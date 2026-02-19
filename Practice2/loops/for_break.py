ca63047c52158d9f0219854c8f7ddf9ac8c7f376
taltusee <e_taltus@kbtu.kz>
2026-02-19 08:08:41 +0500

    added file

---
+++ Practice2/loops/for_break.py
@@ -0,0 +1,26 @@
+#1
+for a in range(1, 10):
+    if a == 5:
+        break
+    print(a)
+#2
+a = int(input())
+for i in range(a):
+    if i == 3:
+        break
+    print(i)
+#3
+for a in range(10):
+    if a == 7:
+        break
+    print(a)
+#4
+for a in range(1, 6):
+    if a == 4:
+        break
+    print(a)
+#5
+for a in range(100):
+    if a == 2:
+        break
+    print(a)
