3828045cb4c71c5899886fcf78ddd1ce2a9c4f1e
taltusee <e_taltus@kbtu.kz>
2026-02-19 07:51:49 +0500

    added file

---
+++ Practice2/if_else/if_elif_else.py
@@ -0,0 +1,42 @@
+#1
+score= 85
+if score>=90:
+    print("Grade A")
+elif score>=75:
+    print("Grade B")
+else:
+    print("Grade C")
+#2
+x=0
+if x>0:
+    print("Positive")
+elif x<0:
+    print("Negative")
+else:
+    print("Zero")
+#3
+day = 3
+if day==1:
+    print("Monday")
+elif day==2:
+    print("Tuesday")
+elif day==3:
+    print("Wednesday")
+else:
+    print("Another day")
+#4
+age=12
+if age>=18:
+    print("Adult")
+elif age>=13:
+    print("Teenager")
+else:
+    print("Child")
+#5
+temp=5
+if temp>25:
+    print("Hot")
+elif temp>10:
+    print("Warm")
+else:
+    print("Cold")
