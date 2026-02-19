7fb8f95bd1511805c9c285ae7553b4df6ef3c0a9
taltusee <e_taltus@kbtu.kz>
2026-02-19 07:54:32 +0500

    added file

---
+++ Practice2/if_else/if_else.py
@@ -0,0 +1,35 @@
+#1
+x = 4
+if x % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+#2
+age = 16
+if age >= 18:
+    print("Access allowed")
+else:
+    print("Access declined")
+
+#3
+password = "1234"
+if password == "admin":
+    print("Access granted")
+else:
+    print("Wrong password")
+
+#4
+a = 10
+b = 5
+if a > b:
+    print("a is greater than b")
+else:
+    print("b is greater or equal to a")
+
+#5
+weather = "sunny"
+if weather == "rain":
+    print("Bad weather")
+else:
+    print("Good weather")
