543f591a1c432288d7bb31455e146669e5b48314
taltusee <e_taltus@kbtu.kz>
2026-02-19 07:53:00 +0500

    added file

---
+++ Practice2/if_else/if_statement.py
@@ -0,0 +1,24 @@
+#1
+x = 10
+if x > 5:
+    print("x  is greater than 5")
+
+#2
+age = 18
+if age >= 18:
+    print("Adult")
+
+#3
+a = 3
+if a == 3:
+    print("a is equal to 3")
+
+#4
+temperature = 30
+if temperature > 25:
+    print("It is hot")
+
+#5
+is_raining = True
+if is_raining:
+    print("take an umbrella")
