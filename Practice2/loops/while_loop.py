17a79ff983614cf1be801e31a7d082df79577432
taltusee <e_taltus@kbtu.kz>
2026-02-19 08:03:38 +0500

    added file

---
+++ Practice2/loops/while_loop.py
@@ -0,0 +1,25 @@
+#1
+a = 1
+while a <= 5:
+    print(a)
+    a += 1
+#2
+a = int(input())
+while a > 0:
+    print(a)
+    a -= 1
+#3
+a = 0
+while a < 3:
+    print("Hi")
+    a += 1
+#4
+a = int(input())
+while a < 10:
+    print(a)
+    a += 2
+#5
+a = 5
+while a > 0:
+    print(a)
+    a -= 1
