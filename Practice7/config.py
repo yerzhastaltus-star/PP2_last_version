9406a2b106380a236b950da3acd9108170eb9b2e
taltusee <e_taltus@kbtu.kz>
2026-04-09 15:17:16 +0500

    config

---
+++ Practice7/config.py
@@ -0,0 +1,8 @@
+def config():
+    return {
+        "host": "localhost",
+        "database": "phonebook_db",
+        "user": "postgres",
+        "password": "12345678",
+        "port": 5432
+    }
