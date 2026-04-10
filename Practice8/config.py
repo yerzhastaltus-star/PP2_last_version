cb9558837eb6c59055c22aa05249fd5cfecfe64c
taltusee <e_taltus@kbtu.kz>
2026-04-10 08:10:26 +0500

    deleted

--- Practice8/config.py
+++
@@ -1,8 +0,0 @@
-def load_config():
-    return {
-        "host": "localhost",
-        "database": "postgres",
-        "user": "postgres",
-        "password": "11111",
-        "port": 5432
-    }
--- Practice8/connect.py
+++
@@ -1,6 +0,0 @@
-import psycopg2
-from config import load_config
-
-
-def connect():
-    return psycopg2.connect(**load_config())
