a8be17741da51bd9fd4919251f49df2fd000efb8
taltusee <e_taltus@kbtu.kz>
2026-04-10 08:20:51 +0500

    deleted

--- Practice7/config.py
+++
@@ -1,8 +0,0 @@
-def config():
-    return {
-        "host": "localhost",
-        "database": "phonebook_db",
-        "user": "postgres",
-        "password": "11111",
-        "port": 5432
-    }
